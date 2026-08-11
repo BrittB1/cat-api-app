ff11b495d1cd7836090fdca51bdd7355278ce672
Brittany Buchanan <157894600+BrittB1@users.noreply.github.com>
2026-08-11 11:01:50 -0400

    Gallery now computes if a cat is favorited or not

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,17 @@
 import Card from "./Card.jsx";
 
-function Gallery({ cats }) {
+function Gallery({ cats, favorites = [], onToggleFavorite }) {
   if (cats.length === 0) return null;
 
   return (
     <section className="gallery">
       {cats.map((cat) => (
-        <Card key={cat.id} cat={cat} />
+        <Card
+          key={cat.id}
+          cat={cat}
+          isFavorite={favorites.some((fav) => fav.imageId === cat.id)}
+          onToggleFavorite={onToggleFavorite}
+        />
       ))}
     </section>
   );
