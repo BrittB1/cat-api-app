6f1dd38af1d9ab426da2c3425f55f6f789dc4073
Brittany Buchanan <157894600+BrittB1@users.noreply.github.com>
2026-08-11 10:54:13 -0400

    Added logic for loading, saving and deleting favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
 import Explorer from "./pages/Explorer.jsx";
 import Favorites from "./pages/Favorites.jsx";
+import { addFavorite, removeFavorite, getFavorites} from "./services/favorites.js";
 
 function App() {
+const [favorite,setFavorites] = useState([]);
+
+  useEffect(() => {
+    async function loadFavorites() {
+      try {
+        const records = await getFavorites();
+        const mapped = records.map((fav) => ({
+          favoriteId: fav.id,
+          imageId: fav.image_id,
+          url: fav.image.url,
+        }));
+        setFavorites(mapped);
+    } catch (error) {
+      console.error ("Couldn't load favorites...",error);
+    }
+  }
+  loadFavorites();
+},[]);
+
+  try {
+    if (existing) {
+      await removeFavorite(existing.favoriteId);
+      setFavorites((prev) => prev, filter((fav) => fav.imageId !== cat.id));
+    } else {
+      const result = await addFavorite(cat.id);
+      setFavorites((prev) => [
+        { favoriteId: result.id, imageId: cat.id, url: cat.url },
+      ]);
+    }
+  } catch (error) {
+    console.error("Whoops! Couldn't update favorites:", error);
+  }
+
   return (
     <BrowserRouter>
       <Navbar />
