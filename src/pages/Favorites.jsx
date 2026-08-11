351cb27c014576028a19ee63f3a4d8d9722507b8
Julianna Vaccaro <julianna.vicente@icloud.com>
2026-08-10 20:28:14 -0400

    Added favorites page conditional rendering

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,17 @@
-
-//This will display saved cats
-
-
+// Displays the user's saved cats
 function Favorites() {
+  const favorites = [];
+
   return (
-    <main>
+    <main className="favorites">
       <h1>Favorite Cats</h1>
-      <p>Your favorite felines will appear here.</p>
+
+      {/* Conditionally displays a message when there are no saved cats */}
+      {favorites.length === 0 ? (
+        <p>You don't have any favorite cats yet.</p>
+      ) : (
+        <p>Your favorite felines will appear here.</p>
+      )}
     </main>
   );
 }
