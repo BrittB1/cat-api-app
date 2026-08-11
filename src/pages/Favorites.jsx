d8fe9c48d2d320e8c6aff6e5fc20c39191ccfed9
Brittany Buchanan <157894600+BrittB1@users.noreply.github.com>
2026-08-11 11:21:46 -0400

    Edited favorites logic to save favorites  and replaced hard coded array

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,6 +1,12 @@
+import Gallery from "../components/Gallery";
+
 // Displays the user's saved cats
-function Favorites() {
-  const favorites = [];
+function Favorites ({favorites,onToggleFavorites}) {
+
+  const favorites = favorites.map((fav) => ({
+    id: fav.imageId,
+    url: fav.url,
+  }));
 
   return (
     <main className="favorites">
@@ -10,10 +16,13 @@ function Favorites() {
       {favorites.length === 0 ? (
         <p>You don't have any favorite cats yet.</p>
       ) : (
-        <p>Your favorite felines will appear here.</p>
+        <Gallery
+        cats={favoriteCats}
+        favorites={favorites}
+        onToggleFavorites={onToggleFavorites}
+        />
       )}
     </main>
   );
 }
-
 export default Favorites;
